877647dfc0b991b0a314eeefc747f7e146e19290
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-03 22:44:21 +0800

    Refactoring of code

--- bouncer.py
+++ bouncer.py
@@ -3,12 +3,13 @@ age = input("How old are you? ")
 
 if age:
 	age=int(age)
-	#18-21 wristband
-	if int(age) >= 18 and int(age) <21:
-		print("You can enter, but need a wristband`")
+	
 	#21, drink, normal entry
-	elif int(age) >=21:
+	if age >=21:
 		print("You are good to enter and can drink!")
+	#18-21 wristband
+	elif  age >=18:
+		print("You can enter, but need a wristband`")
 	#too young, sorry
 	else:
 		print("You're too young. Sorry, little one.")
