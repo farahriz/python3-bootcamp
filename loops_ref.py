fc72d5161ca9a5f649654d439c79b5c51a5eabfc
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-09 22:52:21 +0800

    Added time count

--- loops_ref.py
+++ loops_ref.py
@@ -2,7 +2,8 @@ num = input("How many times do I have to tell you? ")
 num = int(num)
 
 for x in range(num):
-	print("CLEAN UP YOUR ROOM")
+	# print("CLEAN UP YOUR ROOM")
+	print(f"time {x+1}: CLEAN UP YOU ROOM")
 
 # for x in range(1,10):
 # 	print(x)
