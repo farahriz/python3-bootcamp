e997b3aa005043e24018ace7b125fe3f2f62e016
farahriz <41633573+farahriz@users.noreply.github.com>
2019-01-09 22:47:05 +0800

    Repeater excercise added to loop practice

--- loops_ref.py
+++ loops_ref.py
@@ -1,6 +1,12 @@
-for x in range(1,10):
-	print(x)
-	print(x*x)
+print("How many times do I have to tell you? ")
+num = int(input())
 
-for letter in "velvet":
-	print(letter*10)
+for x in range(0,num):
+	print("CLEAN UP YOUR ROOM")
+
+# for x in range(1,10):
+# 	print(x)
+# 	print(x*x)
+
+# for letter in "velvet":
+# 	print(letter*10)
