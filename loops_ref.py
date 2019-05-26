5fefe3986dfe1f7f5533ab833ca92bdf33f50486
farahriz <41633573+farahriz@users.noreply.github.com>
2019-05-26 11:05:02 +0800

    Lambda practice

--- loops_ref.py
+++ loops_ref.py
@@ -1,13 +1,16 @@
-num = input("How many times do I have to tell you? ")
-num = int(num)
+# FOR LOOPS
+	num = input("How many times do I have to tell you? ")
+	num = int(num)
 
-for x in range(num):
-	# print("CLEAN UP YOUR ROOM")
-	print(f"time {x+1}: CLEAN UP YOU ROOM")
+	for x in range(num):
+		# print("CLEAN UP YOUR ROOM")
+		print(f"time {x+1}: CLEAN UP YOU ROOM")
 
-# for x in range(1,10):
-# 	print(x)
-# 	print(x*x)
+	# for x in range(1,10):
+	# 	print(x)
+	# 	print(x*x)
 
-# for letter in "velvet":
-# 	print(letter*10)
+	# for letter in "velvet":
+	# 	print(letter*10)
+
+# WHILE LOOPS
